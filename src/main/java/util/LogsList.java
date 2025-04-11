a08e7e468d95a03419d3496f6f29b108dfc87c7e
Ivan <kuuuuushtanov@gmail.com>
2025-04-11 13:31:10 +0500

    Изменил реализацию deque LinkedBlockingDeque, ускорил subList()

--- src/main/java/util/LogsList.java
+++ src/main/java/util/LogsList.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Список логов не превышающий заданной длины
@@ -13,46 +14,47 @@ public class LogsList<T> extends AbstractList<T> {
 
     public LogsList(int length) {
         this.length = length;
-        deque = new ArrayDeque<>(length);
+        deque = new LinkedBlockingDeque<>(length);
     }
 
     @Override
     public int size() {
-        synchronized (deque) {
-            return deque.size();
-        }
+        return deque.size();
     }
 
     @Override
     public boolean add(T t) {
-        synchronized (deque) {
-            while (deque.size() >= length) {
-                deque.pollFirst();
-            }
-            return deque.add(t);
+        if (deque.size() >= length) {
+            deque.pollFirst();
         }
+        return deque.add(t);
     }
 
     @Override
     public T get(int index) {
-        synchronized (deque) {
-            return deque.stream().toList().get(index);
-        }
+        return deque.stream().toList().get(index);
     }
 
     @Override
     public List<T> subList(int fromIndex, int toIndex) {
-        synchronized (deque) {
-            return deque.stream().toList().subList(fromIndex, toIndex);
+        if (fromIndex < 0 || toIndex > size() || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException();
         }
+        List<T> subList = new ArrayList<>();
+        Iterator<T> iterator = deque.iterator();
+        for (int i = 0; i < toIndex; i++) {
+            if (iterator.hasNext()) {
+                T element = iterator.next();
+                if (i >= fromIndex) {
+                    subList.add(element);
+                }
+            }
+        }
+        return subList;
     }
 
     @Override
     public Iterator<T> iterator() {
-        Iterator<T> syncIterator;
-        synchronized (deque) {
-            syncIterator = deque.iterator();
-        }
-        return syncIterator;
+        return new ArrayList<>(deque).iterator();
     }
 }
